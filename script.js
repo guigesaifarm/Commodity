b475456d4ef085b4e7c2580b1d8ddbffe59eb00e
guigesaifarm <guigesaifarm@gmail.com>
2025-03-10 13:42:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    const apiKey = "SUA_API_KEY"; // Substitua pela sua API Key correta
+    const apiKey = "ded33340fee7453:78hxp3y9yax1d8f"; // Substitua pela sua API Key correta
     const asset = "BTCUSD"; // Identificador do Bitcoin
     const url = `https://api.tradingeconomics.com/markets/ticker/${asset}?c=${apiKey}`;
 
